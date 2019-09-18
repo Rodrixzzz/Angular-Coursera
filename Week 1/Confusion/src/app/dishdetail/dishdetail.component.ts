4bdc543e5e3f1a1036730b922a402c3fee1ee7c9
Cejas <rodrigoantonio.cejas@externos-ar.cencosud.com>
2019-09-18 18:18:13 -0300

    Construcción week 3

--- Week 1/Confusion/src/app/dishdetail/dishdetail.component.ts
+++ Week 1/Confusion/src/app/dishdetail/dishdetail.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Location } from '@angular/common';
 import { Params, ActivatedRoute } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 import { Dish } from '../Shared/Dish';
 import { DishService } from '../services/dish.service';
+import { Comment } from '../Shared/Comment';
 
 
 @Component({
@@ -13,10 +15,34 @@ import { DishService } from '../services/dish.service';
 })
 export class DishdetailComponent implements OnInit {
 
+  commentForm: FormGroup;
+  comment: Comment;
   dish: Dish;
   dishIds: string[];
   prev: string;
   next: string;
+
+  validationMessages = {
+    firstname: {
+      required:      'First Name is required.',
+      minlength:     'First Name must be at least 2 characters long.',
+      maxlength:     'FirstName cannot be more than 25 characters long.'
+    },
+    lastname: {
+      required:      'Last Name is required.',
+      minlength:     'Last Name must be at least 2 characters long.',
+      maxlength:     'Last Name cannot be more than 25 characters long.'
+    },
+    telnum: {
+      required:      'Tel. number is required.',
+      pattern:       'Tel. number must contain only numbers.'
+    },
+    email: {
+      required:      'Email is required.',
+      email:         'Email not in valid format.'
+    },
+  };
+  @ViewChild('fform', {static: false}) CommentFormDirective;
 
   // tslint:disable-next-line: no-shadowed-variable
   constructor(private dishservice: DishService,
